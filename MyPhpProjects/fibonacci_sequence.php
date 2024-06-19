d664c1797024bf126c776e114aba7616058e6bf1
Lama AlAjmah <164665673+ilamaalajmah@users.noreply.github.com>
2024-06-19 20:24:41 +0300

    Update fibonacci_sequence.php

--- MyPhpProjects/fibonacci_sequence.php
+++ MyPhpProjects/fibonacci_sequence.php
@@ -2,29 +2,19 @@
 /* Create a PHP function (Laravel way) that generates a Fibonacci sequence of a specified length. 
 Explain whether you would use recursion or iteration for this task, and provide your implementation.
 */
-namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class FibonacciController extends Controller
+function fibonacci(int $length): array
 {
-    public function generateFibonacci($length)
-    {
-        // Validate input
-        if ($length <= 0) {
-            return response()->json([]);
-        }
-        
-        // Initialize the Fibonacci sequence array
-        $fibonacci = [0, 1];
-        
-        // Generate the Fibonacci sequence
-        for ($i = 2; $i < $length; $i++) {
-            // Calculate the next Fibonacci number
-            $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
-        }
-        
-        return response()->json($fibonacci);
+    $sequence = [0, 1];
+
+    if ($length <= 2) {
+        return array_slice($sequence, 0, $length);
     }
+    while (count($sequence) < $length) {
+        $sequence[] = end($sequence) + prev($sequence);
+    }
+
+    return $sequence;
 }
+$fibonacciSequence = fibonacci(10);
+print_r($fibonacciSequence);
+?>
